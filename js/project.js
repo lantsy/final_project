0f0853d5f5026e341fe7c4e91d9bf037554fe935
lantsy <lahumanlife@gmail.com>
2016-08-26 17:41:18 -0400

    final project 2
    Drop-down menu jQuery

--- js/project.js
+++ js/project.js
@@ -1,72 +1,33 @@
-$(".neighborhood").click(function(){
-	$(".box1").slideDown(200);
-});
-$(".bedroom").click(function(){
-	$(".box2").slideDown(200);
-});
-$(".price").click(function(){
-	$(".box3").slideDown(200);
-});
-$(document).ready(function() {
-  var animating = false;
- 
-  if ($(".slide").length > 1) {
-    $("#next").show();
-  }
-  $("#next").click(fireNext);
 
-  $("#prev").click(firePrevious);
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
 
-  function fireNext() {
-    if (!animating) {
-      animating = true;
-
-      $("#prev").show();
-
-      if ($(".current").next().is(":last-of-type")) {
-        $("#next").hide();
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
-
-      $(".current").next().css("z-index", 1);
-      $(".current").animate({ left: "-100%" }, 500, "easeOutSine", function() {
-        $(".current").css("left", "0").removeClass("current").next().addClass("current").attr("style", "");
-        animating = false;
-      });
     }
   }
+}
+function myFunction() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn2')) {
 
-  function firePrevious() {
-    if (!animating) {
-      animating = true;
-
-      $("#next").show();
-
-      if ($(".current").prev().is(":first-of-type")) {
-        $("#prev").hide();
+    var dropdowns = document.getElementsByClassName("dropdown-content2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown2.classList.contains('show')) {
+        openDropdown2.classList.remove('show');
       }
-
-      $(".current").prev().css("z-index", 1);
-
-      $(".current").animate({ right: "-100%" }, 500, "easeOutSine", function() {
-        $(".current").css("left", "0").removeClass("current").prev().addClass("current").attr("style", "");
-        animating = false;
-      });
     }
   }
-
-  var hammertime = new Hammer(document.getElementById("slideshow"));
-
-  hammertime.on("swipeleft", function() {
-    // console.log("In the swipe left");
-    fireNext();
-  });
-
-  hammertime.on("swiperight", function() {
-    // console.log("In the swipe right");
-    firePrevious();
-  });
-});
-
-
-
-
+}
